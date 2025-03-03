700bea40b3887dc884ae8ef52445e12e8a7fbc9d
Demetre981 <catus2900@gmail.com>
2025-03-03 21:58:42 +0200

    Added some models, registered them at admin, set up the admin pannel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import City, ProgLanguage, Vacancy
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(ProgLanguage)
+admin.site.register(Vacancy)
--- api/models.py
+++ api/models.py
@@ -1,7 +1,53 @@
 from django.db import models
+from .utils import from_cyrillic_to_eng
 
 # Create your models here.
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name="Enter the city where you live", 
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = "Name of city where you live:"
+        verbose_name_plural = "The names of cities where you live"
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self. slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+    
+class ProgLanguage(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Enter your programming language", 
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = "Name of programming language which you use"
+        verbose_name_plural = "The names of programming languages that you use"
+
+    def __str__(self):
+        return self.name
+    
+    # In my opininion, in this func we 
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name="Vacancy title")
+    company_name = models.CharField(max_length=250, verbose_name="Name of the company")
+    description = models.TextField(verbose_name="Vacancy description")
+    city = models.ForeignKey("City", on_delete=models.CASCADE, verbose_name="City")
+    programming_language = models.ForeignKey("ProgLanguage", on_delete=models.CASCADE, verbose_name="Programming Language")
+
+    timestamp = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Vacancy"
+        verbose_name_plural = "Vacancions"
+
+    def __str__(self):
+        return self.title
+    
