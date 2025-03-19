8aa14ac1bc81c8bb395765142a2aabeb107c86cf
Demetre981 <catus2900@gmail.com>
2025-03-19 22:11:56 +0200

    REBASED DB; added Accounts app and auth logic

--- api/forms.py
+++ api/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from api.models import City, ProgLanguage
 
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name="slug", required=False,
+    city = forms.ModelChoiceField(queryset=City.objects.all(), 
+                                  to_field_name="slug", required=False,
                                   widget=forms.Select(attrs={"class": "form-control"}), label="City")
-    proglanguage = forms.ModelChoiceField(queryset=ProgLanguage.objects.all(), to_field_name="name", required=False,
+    proglanguage = forms.ModelChoiceField(queryset=ProgLanguage.objects.all(), 
+                                          to_field_name="name", required=False,
                                           widget=forms.Select(attrs={"class": "form-control"}), label="Programming language")
